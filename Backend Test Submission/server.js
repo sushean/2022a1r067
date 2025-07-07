8ffe9b2df85957269c02e8589309d06ce5f0fb93
Your Name <your.email@example.com>
2025-07-07 12:27:05 +0530

    created main server

---
+++ Backend Test Submission/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const logMiddleware = require('./LoggingMiddleware');
+const shortenerRoutes = require('./shortener');
+
+app.use(express.json());
+app.use(logMiddleware);
+app.use('/', shortenerRoutes);
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});

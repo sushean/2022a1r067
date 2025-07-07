340d1307ca721fa692f07ef5a1a95ebe40251d0d
Your Name <your.email@example.com>
2025-07-07 13:03:58 +0530

    m;n;

--- Logging_Middleware/logMiddleware.js
+++ Logging_Middleware/logMiddleware.js
@@ -26,11 +26,19 @@ async function Log(stack, level, pkg, message) {
         message,
     };
 
-    try 
-    {
-        const response = await axios.post("http://20.244.56.144/evaluation-service/logs", body);
-        console.log("Log sent successfully:", response.status);
-    } 
+    try {
+    const response = await axios.post(
+      "http://20.244.56.144/evaluation-service/logs",
+      body,
+      {
+        headers: {
+          Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiYXVkIjoiaHR0cDovLzIwLjI0NC41Ni4xNDQvZXZhbHVhdGlvbi1zZXJ2aWNlIiwiZW1haWwiOiIyMDIyYTFyMDY3QG1pZXRqYW1tdS5pbiIsImV4cCI6MTc1MTg2OTA5NSwiaWF0IjoxNzUxODY4MTk1LCJpc3MiOiJBZmZvcmQgTWVkaWNhbCBUZWNobm9sb2dpZXMgUHJpdmF0ZSBMaW1pdGVkIiwianRpIjoiYjAxMGY3MDMtZDJjMy00ZTU1LTg1NzEtYmIwOGQwNGY2NzVkIiwibG9jYWxlIjoiZW4tSU4iLCJuYW1lIjoic3VzaGVhbiIsInN1YiI6IjY4NTNmODRlLWQzOGMtNGE1OS1hZDY3LWFiZDIxODYwMjkzMyJ9LCJlbWFpbCI6IjIwMjJhMXIwNjdAbWlldGphbW11LmluIiwibmFtZSI6InN1c2hlYW4iLCJyb2xsTm8iOiIyMDIyYTFyMDY3IiwiYWNjZXNzQ29kZSI6IkZZalFIYyIsImNsaWVudElEIjoiNjg1M2Y4NGUtZDM4Yy00YTU5LWFkNjctYWJkMjE4NjAyOTMzIiwiY2xpZW50U2VjcmV0IjoiU0Rxdk5uTWFoTWZCU2ZhYSJ9.twLPza4uLJaR-dChnBH9DYfK7GNA2-8GWV5FsjrCZXI`
+        }
+      }
+    );
+
+    console.log("Log sent:", response.status);
+  } 
     catch (err) 
     {
         console.error("Failed to send log:", err.message);
